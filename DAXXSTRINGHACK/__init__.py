decd9c02c77474e6b23408883e6dab80ee80d4ca
VIKKY <vikashyadav735556@gmail.com>
2024-06-24 11:28:19 +0530

    Update __init__.py

--- DAXXSTRINGHACK/__init__.py
+++ DAXXSTRINGHACK/__init__.py
@@ -17,7 +17,7 @@ START_PIC = Config.START_PIC
 
 
 if not START_PIC:
-    START_PIC = "https://telegra.ph/file/73cd2c73ff6d19923774c.jpg"
+    START_PIC = "https://telegra.ph/file/af048a5ce51f8f0449adb.jpg"
 
 #rich
 LOG = Console()
@@ -39,7 +39,7 @@ async def DAXXSTRINGHACK():
     header = Table(show_header=True, header_style="bold green")
     header.add_column(LOG_TEXT)
     LOG.print(header)
-    LOG.print(f"[bold cyan]𝐌𝐑.𝐃𝐀𝐗𝐗")
+    LOG.print(f"[bold cyan]𝐃𝐇𝐏𝐑")
     LOG.print("[bold yellow]𝐘𝐨𝐮𝐫 𝐁𝐨𝐭 𝐒𝐭𝐚𝐫𝐭.............")
     await app.start()    
     
