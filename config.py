8c84da657329fcf45c5dda1d77907d6d09928d20
𓄂️𝆺𝅥⃝☠️ ⃪ͥ͢ ᷟ 𝐌͢ꝛ♥️꯭⃝⃨⃜⃝𝐕𝐈𝐊𝐊𝐘༏⃪͢🔥᪵᪳❛𝆺꯭𝅥༎ࠫ <vikashyadav735556@gmail.com>
2025-08-14 17:19:43 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ class Config:
     API_ID = "20136640"
     API_HASH = "32fa482ffd34ceefec26b7ccd73c4fbb"
     #TOKEN = "6521122303:AAGCO3XMjcA0SN5NAi1M0NpmbmMxEtwwYbg"
-    TOKEN = os.environ.get("TOKEN", None)
+    TOKEN = os.environ.get("TOKEN", "7324432991:AAF2GaYhLNqHWz3aIzC3EnoE16BG0OiZvMA")
     MONGO_URL = "mongodb+srv://teamdaxx123:teamdaxx123@cluster0.ysbpgcp.mongodb.net/?retryWrites=true&w=majority"
     START_PIC = "https://telegra.ph/file/73cd2c73ff6d19923774c.jpg"
-    SUDOERS = filters.user(["7147100550"])
+    SUDOERS = filters.user(["7548614955"])
